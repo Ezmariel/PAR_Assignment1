12585b5c71b718dcc55ead65fd32ce13338bd661
Chris Hodgen <bivhitscar@gmail.com>
2021-03-31 16:41:46 +1100

    Bot recognises images and will drive on specific marker

--- search/build/cmake_install.cmake
+++ search/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/chris/assignment/PAR_Assignment1/search/build/gtest/cmake_install.cmake")
+  include("/home/chris/assignment/PAR_Assignment1/search/build/search/cmake_install.cmake")
 
 endif()
 
