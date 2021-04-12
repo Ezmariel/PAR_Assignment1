87c12771ec63bbfa9154e31f265874e9aaf33cd5
Chris Hodgen <bivhitscar@gmail.com>
2021-04-13 09:35:46 +1000

    Remove the build and devel files from git

--- search/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/assignment1/PAR_Assignment1/search/src
-# Build directory: /home/daniel/assignment1/PAR_Assignment1/search/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("search")
